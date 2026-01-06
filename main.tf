985a6f358859dcec03a48a27d9c33fe957d1abbb
ArditCloudDev <ardit@azemi.dev>
2026-01-06 10:53:59 +0100

    Public IP und network interface

--- main.tf
+++ main.tf
@@ -34,3 +34,26 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+# 4. Public IP (Damit die VM aus dem Internet erreichbar ist)
+resource "azurerm_public_ip" "public_ip" {
+  name                = "vm-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# 5. Network Interface (Die virtuelle Netzwerkkarte)
+resource "azurerm_network_interface" "nic" {
+  name                = "vm-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
