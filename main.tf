3a6d1a415dcc4d1c41165a92d53f17b4726656f3
ArditCloudDev <ardit@azemi.dev>
2026-01-06 10:56:29 +0100

    adding network security group allowing SSH

--- main.tf
+++ main.tf
@@ -57,3 +57,29 @@ resource "azurerm_network_interface" "nic" {
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
+
+# 6. Network Security Group (Die Firewall)
+resource "azurerm_network_security_group" "nsg" {
+  name                = "vm-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  # Regel: SSH erlauben
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# 7. Verknüpfung von NIC und NSG
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
